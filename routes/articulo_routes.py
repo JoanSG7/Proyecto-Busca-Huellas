88a5ad28e80ae8736846ed5415920175bc7a6098
juandavid1407 <davidrigrillo10@gmail.com>
2026-05-28 06:47:29 -0500

    Actualiza proyecto y base de datos

--- routes/articulo_routes.py
+++ routes/articulo_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 # Importamos las funciones del controlador
-from controllers.auth_controller import mostrar_lista_articulos, mostrar_articulo_completo
+from controllers.auth_controller import borrar_articulo, editar_articulo, mostrar_lista_articulos, mostrar_articulo_completo, registrar_articulo
+from controllers.security import admin_required
 
 articulo_bp = Blueprint('articulo', __name__)
 
@@ -9,6 +10,21 @@ articulo_bp = Blueprint('articulo', __name__)
 def lista_articulos():
     return mostrar_lista_articulos()
 
-@articulo_bp.route('/articulo')
-def articulo_completo():
-    return mostrar_articulo_completo()
+@articulo_bp.route('/articulo/<int:id_articulo>')
+def articulo_completo(id_articulo):
+    return mostrar_articulo_completo(id_articulo)
+
+@articulo_bp.route('/crear', methods=['POST'])
+@admin_required
+def crear():
+    return registrar_articulo()
+
+@articulo_bp.route('/editar/<int:id_articulo>', methods=['POST'])
+@admin_required
+def editar(id_articulo):
+    return editar_articulo(id_articulo)
+
+@articulo_bp.route('/eliminar/<int:id_articulo>', methods=['POST'])
+@admin_required
+def eliminar(id_articulo):
+    return borrar_articulo(id_articulo)
